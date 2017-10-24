723bb62812febde3e7ff669eacb3c37f1f7ffaa4
Javi <javier.fernandez3@live.u-tad.com>
2017-10-24 21:58:29 +0200

    Hago un commit porque nos echan de clase que sino...

--- src/app/micanvas/actividad1.ts
+++ src/app/micanvas/actividad1.ts
@@ -81,21 +81,22 @@ export class Actividad1 implements EventsAdminListener{
         console.log(this.arPreguntas[0]);
         console.log(this.arPreguntas[1]);
         console.log(this.arPreguntas[2]);
+
+
         //RESPUESTAS
         this.arRespuestas = new Array<String[]>();
         let arrAux :String[] = ["Un IceBerg","Un Inglés en Mallorca","Un tiranosaurius Rex albino","Una nevera"]
-       /*this.arRespuestas = [[this.arPreguntas[0]],["Un IceBerg","Un Inglés en Mallorca","Un tiranosaurius Rex albino","Una nevera"]],
-        [[this.arPreguntas[1]],["0.5","2","8","0.000000001"]],
-       [[this.arPreguntas[2]],["13 min","0 min","Lo que dura una partida del pubg","Lo que tarda Tuvilla en marearse en el pubg"]];*/
         this.arRespuestas[0] = arrAux;
         arrAux = ["0.5","2","8","0.000000001"];
         this.arRespuestas[1] = arrAux;
         arrAux = ["13 min","0 min","Lo que dura una partida del pubg","Lo que tarda Tuvilla en marearse en el pubg"];
         this.arRespuestas[2] = arrAux;
-        console.log(this.arRespuestas);
+        
+
+
        //RESPUESTAS CORRECTAS
         this.arRespuestasCorrectas= [this.arRespuestas[0][3],this.arRespuestas[1][2],this.arRespuestas[2][1]];
-        console.log(this.arRespuestasCorrectas);
+    
 
         
     }
